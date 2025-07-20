9f1bd04235e61fd522f3b7679ff9780023f1511a
Aditya Nayak <19adityanayak@gmail.com>
2025-07-20 18:22:52 +0530

    update database configuration

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URL);
     console.log('✅ MongoDB connected successfully ');
   } catch (error) {
     console.error('❌ MongoDB connection failed:', error.message);
